1b2590d5c57ab037209713550df2d8fb30480280
Artur_Ilinyh <ynskpo@gmail.com>
2023-02-07 10:31:04 +0700

    Подключение шапки и низа из файла, позволить редактировать один хедер для 100 страниц

---
+++ blocks/header.php
@@ -0,0 +1,12 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom container">
+	<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+		<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+		<span class="fs-4">PHP project</span>
+	</a>
+
+	<ul class="nav nav-pills">
+		<li class="nav-item"><a href="#" class="nav-link">Главная</a></li>
+		<li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
+	</ul>
+	<a class="btn btn-outline-primary" href="#">Войти</a>
+</header>
