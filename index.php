f12b3f79f9b075086ec3da369f599472c62ae3a1
Artur_Ilinyh <ynskpo@gmail.com>
2023-02-07 09:54:08 +0700

    Вывод через цикл PHP

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP вебсайт</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"><!--Бутстреп-->
+</head>
+<body>
+
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+	<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+		<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+		<span class="fs-4">PHP project</span>
+	</a>
+
+	<ul class="nav nav-pills">
+		<li class="nav-item"><a href="#" class="nav-link">Главная</a></li>
+		<li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
+	</ul>
+	<a class="btn btn-outline-primary" href="#">Войти</a>
+</header>
+
+<div class ="container mt-5">
+			<h3>Как создать первый сайт на PHP</h3>
+</div>
+
+<div class="cars mb-4 shadow-sm">
+<?php
+	for($i = 0; $i<5; $i++):
+	?>     
+	<div class="row flex-lg-row-reverse align-items-center g-5 py-5 m-lg-2 ">
+	<div class="col-10 col-sm-8 col-lg-6">
+        <img src="img/bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+	<div class="col-lg-6">
+		<h1 class="display-5 fw-bold lh-1 mb-3">Познакомьтесь с bootstrap</h1>
+		<p class="lead">Все статичные сайты можно копиривоать на свои хостинги, и, например, монитизировать. На bootstrap-е можно найти в примерах шаблоны для лубого сайта, подкючив библиотеку бутстрепа, они будут прекрасно работать!)</p>
+		<div class="d-grid gap-2 d-md-flex justify-content-md-start">
+			<button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+			<button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+		</div>
+	</div>
+</div>
+<?php endfor; ?>
+</div>
+
+
+</body>
+</html>
